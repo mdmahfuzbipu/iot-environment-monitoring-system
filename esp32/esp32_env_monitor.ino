5105fcbbc2809511ce0d06097e6d76f91c0a8d27
mdmahfuzbipu <mdmahfuzbipu@gmail.com>
2025-11-18 16:08:53 +0600

    Add esp32 module code

---
+++ esp32/esp32_env_monitor.ino
@@ -0,0 +1,81 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <DHT.h>
+#include <Adafruit_BMP280.h>
+
+// ------------------ SENSOR SETUP ------------------
+#define DHTPIN 4
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+
+Adafruit_BMP280 bmp;
+const int mqPin = 34;
+
+// ------------------ WIFI & SERVER ------------------
+const char *ssid = "C.A. & O. Lab";
+const char *password = "123456789";
+const char *serverName = "http://192.168.0.105:8000/api/data/";
+
+// ------------------ SETUP ------------------
+void setup()
+{
+    Serial.begin(115200);
+
+    dht.begin();
+
+    if (!bmp.begin())
+    {
+        Serial.println("BMP280 not found! Pressure will be sent as null.");
+    }
+
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting to WiFi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println("\nConnected to WiFi!");
+}
+
+// ------------------ LOOP ------------------
+void loop()
+{
+    // Read sensors
+    float temperature = dht.readTemperature();
+    float humidity = dht.readHumidity();
+    float pressure = bmp.begin() ? bmp.readPressure() / 100.0F : NAN; // hPa
+    float air_quality = analogRead(mqPin);
+
+    // Prepare JSON
+    String temperatureStr = isnan(temperature) ? "null" : String(temperature, 2);
+    String humidityStr = isnan(humidity) ? "null" : String(humidity, 2);
+    String pressureStr = isnan(pressure) ? "null" : String(pressure, 2);
+    String airStr = isnan(air_quality) ? "null" : String(air_quality, 0);
+
+    String jsonData = "{\"temperature\":" + temperatureStr +
+                      ",\"humidity\":" + humidityStr +
+                      ",\"air_quality\":" + airStr +
+                      ",\"pressure\":" + pressureStr + "}";
+
+    // Send to Django
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        HTTPClient http;
+        http.begin(serverName);
+        http.addHeader("Content-Type", "application/json");
+
+        int httpResponseCode = http.POST(jsonData);
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+
+        http.end();
+    }
+    else
+    {
+        Serial.println("WiFi disconnected! Reconnecting...");
+        WiFi.reconnect();
+    }
+
+    delay(5000);
+}
